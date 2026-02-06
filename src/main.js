017ebaa31c04f2b73fb3db73e2a2dae0e9f6f0ed
tcolonhezi <thiago.colonhezi@gmail.com>
2026-02-05 21:43:10 -0300

    feat: add SVG assets for calendar, cancel, logo, morning, night, person, and scissors
    - Added assets
    
    chore: configure dayjs for date handling
    
    - Imported dayjs and set locale to Portuguese (pt-br).
    - Added dayjs configuration in main.js.
    
    build: setup webpack configuration
    
    - Configured webpack for development with entry and output settings.
    - Added HtmlWebpackPlugin for HTML file generation.
    - Included CopyWebpackPlugin to copy assets to the dist folder.
    - Set up loaders for CSS and JavaScript files.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Configuração do dayjs
+import "./libs/dayjs";
+
+//CSS
+import "./styles/global.css";
+import "./styles/schedule.css";
+import "./styles/form.css";
+
+import dayjs from "dayjs";
+
+console.log(dayjs().format("DD HH:mm"));
